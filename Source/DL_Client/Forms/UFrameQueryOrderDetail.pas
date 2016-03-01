9256424c653b02f90f9f78d1cbdcbad944c554b4
liyafeng168 <fendou116688@163.com>
2016-03-01 14:38:19 +0800

    *.原材料明细

---
+++ Source/DL_Client/Forms/UFrameQueryOrderDetail.pas
@@ -0,0 +1,230 @@
+{*******************************************************************************
+  作者: fendou116688@163.com 2015/8/10
+  描述: 采购验收明细
+*******************************************************************************}
+unit UFrameQueryOrderDetail;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  UFrameNormal, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, cxStyles, cxCustomData, cxFilter, cxData,
+  cxDataStorage, cxEdit, DB, cxDBData, cxContainer, Menus, dxLayoutControl,
+  cxMaskEdit, cxButtonEdit, cxTextEdit, ADODB, cxLabel, UBitmapPanel,
+  cxSplitter, cxGridLevel, cxClasses, cxGridCustomView,
+  cxGridCustomTableView, cxGridTableView, cxGridDBTableView, cxGrid,
+  ComCtrls, ToolWin;
+
+type
+  TfFrameOrderDetailQuery = class(TfFrameNormal)
+    cxtxtdt1: TcxTextEdit;
+    dxLayout1Item5: TdxLayoutItem;
+    EditDate: TcxButtonEdit;
+    dxLayout1Item6: TdxLayoutItem;
+    EditCustomer: TcxButtonEdit;
+    dxLayout1Item8: TdxLayoutItem;
+    cxtxtdt2: TcxTextEdit;
+    dxLayout1Item1: TdxLayoutItem;
+    pmPMenu1: TPopupMenu;
+    mniN1: TMenuItem;
+    cxtxtdt3: TcxTextEdit;
+    dxLayout1Item2: TdxLayoutItem;
+    cxtxtdt4: TcxTextEdit;
+    dxLayout1Item3: TdxLayoutItem;
+    EditTruck: TcxButtonEdit;
+    dxLayout1Item4: TdxLayoutItem;
+    EditBill: TcxButtonEdit;
+    dxLayout1Item7: TdxLayoutItem;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    procedure EditDatePropertiesButtonClick(Sender: TObject;
+      AButtonIndex: Integer);
+    procedure EditTruckPropertiesButtonClick(Sender: TObject;
+      AButtonIndex: Integer);
+    procedure mniN1Click(Sender: TObject);
+    procedure N2Click(Sender: TObject);
+    procedure N3Click(Sender: TObject);
+    procedure N4Click(Sender: TObject);
+  private
+    { Private declarations }
+  protected
+    FStart,FEnd: TDate;
+    FTimeS,FTimeE: TDate;
+    //时间区间
+    FJBWhere: string;
+    //交班条件 
+    procedure OnCreateFrame; override;
+    procedure OnDestroyFrame; override;
+    function InitFormDataSQL(const nWhere: string): string; override;
+    //查询SQL
+  public
+    { Public declarations }
+    class function FrameID: integer; override;
+  end;
+
+implementation
+
+{$R *.dfm}
+uses
+  IniFiles, ULibFun, UMgrControl, UFormDateFilter, USysPopedom, USysBusiness,
+  UBusinessConst, USysConst, USysDB;
+
+class function TfFrameOrderDetailQuery.FrameID: integer;
+begin
+  Result := cFI_FrameOrderDetailQuery;
+end;
+
+procedure TfFrameOrderDetailQuery.OnCreateFrame;
+begin
+  inherited;
+  FTimeS := Str2DateTime(Date2Str(Now) + ' 00:00:00');
+  FTimeE := Str2DateTime(Date2Str(Now) + ' 00:00:00');
+
+  FJBWhere := '';
+  InitDateRange(Name, FStart, FEnd);
+end;
+
+procedure TfFrameOrderDetailQuery.OnDestroyFrame;
+begin
+  SaveDateRange(Name, FStart, FEnd);
+  inherited;
+end;
+
+function TfFrameOrderDetailQuery.InitFormDataSQL(const nWhere: string): string;
+begin
+  EditDate.Text := Format('%s 至 %s', [Date2Str(FStart), Date2Str(FEnd)]);
+  Result := 'Select *,(P_MValue-P_PValue-P_KZValue) as P_NetWeight ' +
+            'From $POrder ';
+  //xxxxxx
+
+  if FJBWhere = '' then
+  begin
+    Result := Result + 'Where (P_OutFact>=''$S'' and P_OutFact <''$End'')';
+
+    if nWhere <> '' then
+      Result := Result + ' And (' + nWhere + ')';
+    //xxxxx
+  end else
+  begin
+    Result := Result + ' Where (' + FJBWhere + ')';
+  end;
+
+  Result := MacroValue(Result, [MI('$POrder', sTable_PurchInfo),
+            MI('$S', Date2Str(FStart)),
+            MI('$End', Date2Str(FEnd + 1))]);
+  //xxxxx
+end;
+
+
+//Desc: 日期筛选
+procedure TfFrameOrderDetailQuery.EditDatePropertiesButtonClick(Sender: TObject;
+  AButtonIndex: Integer);
+begin
+  if ShowDateFilterForm(FStart, FEnd) then InitFormData(FWhere);
+end;
+
+//Desc: 执行查询
+procedure TfFrameOrderDetailQuery.EditTruckPropertiesButtonClick(Sender: TObject;
+  AButtonIndex: Integer);
+begin
+  if Sender = EditCustomer then
+  begin
+    EditCustomer.Text := Trim(EditCustomer.Text);
+    if EditCustomer.Text = '' then Exit;
+
+    FWhere := 'P_ProPY like ''%%%s%%'' Or P_ProName like ''%%%s%%''';
+    FWhere := Format(FWhere, [EditCustomer.Text, EditCustomer.Text]);
+    InitFormData(FWhere);
+  end else
+
+  if Sender = EditTruck then
+  begin
+    EditTruck.Text := Trim(EditTruck.Text);
+    if EditTruck.Text = '' then Exit;
+
+    FWhere := 'P_Truck like ''%%%s%%''';
+    FWhere := Format(FWhere, [EditTruck.Text]);
+    InitFormData(FWhere);
+  end;
+
+  if Sender = EditBill then
+  begin
+    EditBill.Text := Trim(EditBill.Text);
+    if EditBill.Text = '' then Exit;
+
+    FWhere := 'P_ID like ''%%%s%%''';
+    FWhere := Format(FWhere, [EditBill.Text]);
+    InitFormData(FWhere);
+  end;
+end;
+
+//Desc: 交接班查询
+procedure TfFrameOrderDetailQuery.mniN1Click(Sender: TObject);
+begin
+  if ShowDateFilterForm(FTimeS, FTimeE, True) then
+  try
+    FJBWhere := '(P_OutFact>=''%s'' and P_OutFact <''%s'')';
+    FJBWhere := Format(FJBWhere, [DateTime2Str(FTimeS), DateTime2Str(FTimeE)]);
+    InitFormData('');
+  finally
+    FJBWhere := '';
+  end;
+end;
+//------------------------------------------------------------------------------
+//Date: 2015/8/13
+//Parm: 
+//Desc: 查询未完成
+procedure TfFrameOrderDetailQuery.N2Click(Sender: TObject);
+begin
+  inherited;
+  try
+    FJBWhere := '(P_OutFact Is Null)';
+    InitFormData('');
+  finally
+    FJBWhere := '';
+  end;
+end;
+//------------------------------------------------------------------------------
+//Date: 2015/8/13
+//Parm: 
+//Desc: 删除未完成记录
+procedure TfFrameOrderDetailQuery.N3Click(Sender: TObject);
+var nStr: string;
+begin
+  inherited;
+  if cxView1.DataController.GetSelectedCount > 0 then
+  begin
+    nStr := SQLQuery.FieldByName('P_ID').AsString;
+    if not QueryDlg('确认删除该订单么?', sWarn) then Exit;
+
+    //nSQL := MacroValue()
+  end;
+
+  N2.Click;
+end;
+
+procedure TfFrameOrderDetailQuery.N4Click(Sender: TObject);
+begin
+  inherited;
+  if ShowDateFilterForm(FTimeS, FTimeE, True) then
+  try
+    if Sender = N4 then   //过空时间
+       FJBWhere := '(P_PDate>=''%s'' and P_PDate <''%s'')'
+    else
+    if Sender = N5 then   //进厂时间
+      FJBWhere := '(P_InTime>=''%s'' and P_InTime <''%s'')';
+
+    FJBWhere := Format(FJBWhere, [DateTime2Str(FTimeS), DateTime2Str(FTimeE)]);
+    InitFormData('');
+  finally
+    FJBWhere := '';
+  end;
+end;
+
+initialization
+  gControlManager.RegCtrl(TfFrameOrderDetailQuery, TfFrameOrderDetailQuery.FrameID);
+end.
